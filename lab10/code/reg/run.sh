ec0bf2c5bad380bf9540e4482615c0b487cb046c
KDAIer <md1425529116@gmail.com>
2025-05-30 09:17:44 +0800

    finished lab10

--- lab10/code/reg/run.sh
+++ lab10/code/reg/run.sh
@@ -7,21 +7,21 @@ sizes=(512 1024 2048)
 dims=(8 16 32)
 
 for d in "${dims[@]}"; do
-  # 编译三份可执行：RowShared_d、ColShared_d、BlockShared_d
-  nvcc -DTILE_DIM=$d -O2 row.cu   -o RowShared_$d
-  nvcc -DTILE_DIM=$d -O2 column.cu   -o ColShared_$d
-  nvcc -DTILE_DIM=$d -O2 block.cu -o BlockShared_$d
+  # 编译三份可执行：RowReg_d、ColReg_d、BlockReg_d
+  nvcc -DTILE_DIM=$d -O2 row.cu   -o RowReg_$d
+  nvcc -DTILE_DIM=$d -O2 column.cu   -o ColReg_$d
+  nvcc -DTILE_DIM=$d -O2 block.cu -o BlockReg_$d
 
   for N in "${sizes[@]}"; do
     # 运行并抓取时间
-    t1=$(./RowShared_$d $N $N $N $d $d | awk '{print $2}')
-    t2=$(./ColShared_$d $N $N $N $d $d | awk '{print $2}')
-    t3=$(./BlockShared_$d $N $N $N $d $d | awk '{print $2}')
+    t1=$(./RowReg_$d $N $N $N $d $d | awk '{print $2}')
+    t2=$(./ColReg_$d $N $N $N $d $d | awk '{print $2}')
+    t3=$(./BlockReg_$d $N $N $N $d $d | awk '{print $2}')
 
     # 写入结果
-    echo -e "RowShared\t${N}×${N}\t${d}×${d}\t${t1}" >> $OUTPUT
-    echo -e "ColShared\t${N}×${N}\t${d}×${d}\t${t2}" >> $OUTPUT
-    echo -e "BlockShared\t${N}×${N}\t${d}×${d}\t${t3}" >> $OUTPUT
+    echo -e "RowReg\t${N}×${N}\t${d}×${d}\t${t1}" >> $OUTPUT
+    echo -e "ColReg\t${N}×${N}\t${d}×${d}\t${t2}" >> $OUTPUT
+    echo -e "BlockReg\t${N}×${N}\t${d}×${d}\t${t3}" >> $OUTPUT
   done
 done
 
